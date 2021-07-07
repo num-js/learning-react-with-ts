bd17d73a63df2c25cb4c6a787b215d40419216d2
Numan Ahmed <mdnmnahmed@gmail.com>
2021-07-07 09:15:55 +0530

    States type defines in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number, setNumber] = useState<Number | string | boolean>(10);
+    const [anyData, setAnyData] = useState<any>(10);
+
+    const changeNumber = () => {
+        setNumber(10);
+    }
+
+    const changeName = () => {
+        setNumber('Numan');
+    }
+
+    const changeBoolean = () => {
+        setNumber(true);
+    }
+
+    const changeAnyData = () => {
+        setAnyData(false);
+        setAnyData('Numan');
+        setAnyData(100);
+    }
+
+    return (
+        <div className="App">
+            Hii
+        </div>
+    );
 }
 
 export default App;
