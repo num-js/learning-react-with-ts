1c9090ca64a2c44f40156559946c93e00bccefe6
Numan Ahmed <mdnmnahmed@gmail.com>
2021-08-09 20:41:07 +0530

    different types of Data Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
 const App = () => {
+
+    let name: string = "Numan";
+    let age: number = 22;
+    let isDev: boolean = true;
+    let langs: string[];
+
+    // Arrays
+    let role: [number, string];
+    role = [10, 'data']
+
+    let myData: number | string;
+
+    let someData: any; //Not Recommended
+    let someMoreData: unknown; //Recommended
+
+    // Objects
+    type Dev = {    //Interface
+        name: string,
+        age?: number
+    }
+    let devData: Dev = {    //Object with Dev Type/Interface
+        name: "Numan Ahmed",
+    }
+
+    // Array
+    let lotsOfDev: Dev[]; //Array with Dev Type/Interface
+
+
+    const myDataFun = (name: string) => void {
+        console.log(name)
+    }
+
+    myDataFun('numan');
+
     return (
         <>
-            Hello Numan
+            <h1>
+                {devData.name}
+            </h1>
         </>
     );
 }
